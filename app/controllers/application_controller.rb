23303eadb300c3498f111a6980612156e69485d8
Alec Johnson <alecjohnson55@gmail.com>
2017-03-14 14:04:44 -0700

    add bang in log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def log_in!(user)
-    session[:session_token] = user.reset_session_token
+    session[:session_token] = user.reset_session_token!
   end
 
   def current_user
