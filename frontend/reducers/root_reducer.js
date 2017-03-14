74c9357bd7c5f11aaadf1957ff4142c5559c4723
Alec Johnson <alecjohnson55@gmail.com>
2017-03-14 16:44:44 -0700

    add root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,2 +1,10 @@
 import { combineReducers } from 'redux';
-import SessionReducer
+import SessionReducer from './session_reducer';
+import ErrorsReducer from './errors_reducer';
+
+const RootReducer = combineReducers({
+  session: SessionReducer,
+  errors: ErrorsReducer
+});
+
+export default RootReducer;
