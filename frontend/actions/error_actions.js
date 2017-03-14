59e1c8236f398a2bed3abaa5e4b636a8c6894763
Alec Johnson <alecjohnson55@gmail.com>
2017-03-14 16:50:00 -0700

    fix formatting, alphabetize imports

--- frontend/actions/error_actions.js
+++ frontend/actions/error_actions.js
@@ -1,11 +1,10 @@
-export const RECEIVE_SIGN_UP_ERRORS = 'RECEIVE_SIGN_UP_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 export const RECEIVE_LOG_IN_ERRORS = 'RECEIVE_LOG_IN_ERRORS';
 export const RECEIVE_LOG_OUT_ERRORS = 'RECEIVE_LOG_OUT_ERRORS';
-export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const RECEIVE_SIGN_UP_ERRORS = 'RECEIVE_SIGN_UP_ERRORS';
 
-export const receiveSignUpErrors = errors => ({
-  type: RECEIVE_SIGN_UP_ERRORS,
-  errors
+export const clearErrors = errors => ({
+  type: CLEAR_ERRORS
 });
 
 export const receiveLogInErrors = errors => ({
@@ -18,6 +17,7 @@ export const receiveLogOutErrors = errors => ({
   errors
 });
 
-export const clearErrors = errors => ({
-  type: CLEAR_ERRORS
+export const receiveSignUpErrors = errors => ({
+  type: RECEIVE_SIGN_UP_ERRORS,
+  errors
 });
