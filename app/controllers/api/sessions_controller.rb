e541c25c1ba7556d16f5d3d45a54d1a861a75ebb
Alec Johnson <alecjohnson55@gmail.com>
2017-03-14 13:51:39 -0700

    add create/delete routes to session controller for login/logout

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,31 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = user.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password])
+
+    if @user
+      log_in!(@user)
+      render "api/users/show"
+    else
+      render json: ["invalid username/password combination"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      log_out!
+      render "api/users/show"
+    else
+      render json: ["nobody signed in"], status: 404
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:username, :password)
+  end
 end
