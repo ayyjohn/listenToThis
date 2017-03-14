3239cccfebc75b33aea2e09416ef38bb98347022
Alec Johnson <alecjohnson55@gmail.com>
2017-03-14 14:57:26 -0700

    add action testers to window

--- frontend/listen_to_this.jsx
+++ frontend/listen_to_this.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import { logIn, logOut, signUp } from './actions/session_actions';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Listen to This!</h1>, root);
 });
+
+// window.logIn = logIn;
+// window.logOut = logOut;
+// window.signUp = signUp;
