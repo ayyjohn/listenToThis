b8f99defc5d30f5c8c9a350e1986df480de2a7cc
Alec Johnson <alecjohnson55@gmail.com>
2017-03-15 14:57:02 -0700

    add redux-dev tools to middleware, add splash photo

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,9 +1,14 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
 import RootReducer from '../reducers/root_reducer';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const configureStore = (preloadedState = {}) => (
-  createStore(RootReducer, preloadedState, applyMiddleware(thunk))
+  createStore(RootReducer,
+    preloadedState,
+    composeEnhancers(
+      applyMiddleware(thunk))
+    )
 );
 
 export default configureStore;
