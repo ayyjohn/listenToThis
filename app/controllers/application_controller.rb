bcf80cd7d96d806266a6f46cda036cd285657445
Alec Johnson <alecjohnson55@gmail.com>
2017-03-15 15:19:27 -0700

    update gemfilet to include paperclip, remove question mark to fix atom highlighting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in
 
   def log_in!(user)
     session[:session_token] = user.reset_session_token!
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
-  def logged_in?
+  def logged_in
     !!current_user
   end
 
