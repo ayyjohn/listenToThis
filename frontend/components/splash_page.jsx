46e760059825752834a286d408d150fc76c3b8a0
Alec Johnson <alecjohnson55@gmail.com>
2017-03-14 19:01:14 -0700

    add log in form, sign up form, containers

--- frontend/components/splash_page.jsx
+++ frontend/components/splash_page.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const SplashPage = () => (
   <div>
     <h1>Listen to This!!</h1>
+    <Link to="/signup">Sign Up</Link>
+    <Link to="/login">Log In</Link>
   </div>
 );
 
