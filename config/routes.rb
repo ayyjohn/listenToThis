9677c29c08d6cca82986c3f7d4f2ef8dfe80c61a
Alec Johnson <alecjohnson55@gmail.com>
2017-03-16 11:07:15 -0700

    remove nesting of tracks in planning for new approach with search bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create] do
-      resources :tracks, only: [:index]
-    end
-    resources :tracks, only: [:create, :show, :update, :destroy]
+    resources :users, only: [:create]
+    resources :tracks, except: [:new, :edit]
     resource :session, only: [:create, :destroy]
   end
 
