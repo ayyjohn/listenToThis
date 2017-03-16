ad23e436881f0e65ca9f974535377c7a63fc6ad8
Alec Johnson <alecjohnson55@gmail.com>
2017-03-16 09:25:08 -0700

    remove overlay whiteout from screen, add fade in for modals

--- frontend/components/splash_page.jsx
+++ frontend/components/splash_page.jsx
@@ -5,6 +5,12 @@ import Modal from 'react-modal';
 import LogInFormContainer from './log_in_form_container';
 import SignUpFormContainer from './sign_up_form_container';
 
+const fadeStyles = {
+  overlay: {
+    backgroundColor: 'rgba(255, 255, 255, 0)'
+  }
+};
+
 class SplashPage extends React.Component {
 
   constructor(props) {
@@ -12,6 +18,8 @@ class SplashPage extends React.Component {
     this.state = {
       logInModalIsOpen: false,
       signUpModalIsOpen: false,
+      logInModalIsShown: false,
+      signUpModalIsShown: false
     };
     this.openLogInModal = this.openLogInModal.bind(this);
     this.closeLogInModal = this.closeLogInModal.bind(this);
@@ -21,18 +29,22 @@ class SplashPage extends React.Component {
 
   openLogInModal() {
     this.setState({ logInModalIsOpen: true });
+    setTimeout(() => this.setState({ logInModalIsShown: true }), 0);
   }
 
   closeLogInModal() {
     this.setState({ logInModalIsOpen: false });
+    this.setState({ logInModalIsShown: false });
   }
 
   openSignUpModal() {
     this.setState({ signUpModalIsOpen: true });
+    setTimeout(() => this.setState({ signUpModalIsShown: true }), 0);
   }
 
   closeSignUpModal() {
     this.setState({ signUpModalIsOpen: false });
+    this.setState({ signUpModalIsShown: false });
   }
 
   componentWillMount() {
@@ -54,18 +66,20 @@ class SplashPage extends React.Component {
           <Link id="splash-get-started" onClick={ this.openSignUpModal }>Get Started</Link>
         </section>
         <Modal
-          className="splash-sign-up-modal"
+          className={ this.state.signUpModalIsShown ? "splash-sign-up-modal-show" : "splash-sign-up-modal-hide" }
           isOpen={ this.state.signUpModalIsOpen }
           onRequestClose={ this.closeSignUpModal }
           contentLabel="Modal"
+          style={ fadeStyles }
           >
           <SignUpFormContainer />
         </Modal>
         <Modal
-          className="splash-log-in-modal"
+          className={this.state.logInModalIsShown ? "splash-log-in-modal-show" : "splash-log-in-modal-hide" }
           isOpen={ this.state.logInModalIsOpen }
           onRequestClose={ this.closeLogInModal }
           contentLabel="Modal"
+          style={ fadeStyles }
           >
           <LogInFormContainer />
         </Modal>
