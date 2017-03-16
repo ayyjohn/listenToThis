d8113f3d97897ee82600b60c702a3fc530bec65d
Alec Johnson <alecjohnson55@gmail.com>
2017-03-16 16:40:30 -0700

    use certain version of paperclip, add aws sdk

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,15 +2,19 @@
 #
 # Table name: tracks
 #
-#  id           :integer          not null, primary key
-#  name         :string           not null
-#  image_url    :string
-#  release_date :date             not null
-#  description  :text
-#  genre        :string           not null
-#  user_id      :string           not null
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id                         :integer          not null, primary key
+#  name                       :string           not null
+#  image_url                  :string
+#  release_date               :date             not null
+#  description                :text
+#  genre                      :string           not null
+#  user_id                    :string           not null
+#  created_at                 :datetime         not null
+#  updated_at                 :datetime         not null
+#  album_artwork_file_name    :string
+#  album_artwork_content_type :string
+#  album_artwork_file_size    :integer
+#  album_artwork_updated_at   :datetime
 #
 
 class Track < ApplicationRecord
@@ -18,5 +22,8 @@ class Track < ApplicationRecord
   validates :name, :release_date, :genre, :user, presence: true
 
   belongs_to :user
-  has_many :comments, dependent: :destroy
+  has_many :comments #, dependent: :destroy
+
+  has_attached_file :album_artwork, default_url: "https://s3-us-west-1.amazonaws.com/listentothis-dev/no_album.png"
+  validates_attachment_content_type :album_artwork, content_type: /\Aimage\/.*\Z/
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-User.create(username: "guest", password: "password", location: "New York, NY", bio: "I love life and music.")
-User.create(username: "ayyjohn", password: "password", location: "San Francisco, CA", bio: "My inspirations include Kanye West and Kanye West.")
-User.create(username: "barkbark", password: "password", location: "San Luis Obispo, CA", bio: "I like sadder music. Slow stuff helps me concentrate.")
-User.create(username: "warriorprincess", password: "password", location: "San Diego, CA", bio: "I really only like oldies, new music is bad!")
-User.create(username: "thedykm", password: "password", location: "Seattle, WA", bio: "I can vibe to whatever, but let's be real, the less people who've heard of it the cooler it is.")
-User.create(username: "ridersofrray", password: "password", location: "Seattle, WA", bio: "When I find a song I like I play it for two weeks until I'm sick of it. I can't help it.")
-User.create(username: "ridethebus", password: "password", location: "San Francisco, CA", bio: "I like trance mixes. They help me stay up through the night working.")
-User.create(username: "strozzapreti", password: "password", location: "Montreal, Canada", bio: "I like funk and rhythm stuff, but I have a keen ear for jazz too. I've been known to enjoy a really deep rap song or two.")
+u1 = User.create!(username: "guest", password: "password", location: "New York, NY", bio: "I love life and music.")
+u2 = User.create(username: "ayyjohn", password: "password", location: "San Francisco, CA", bio: "My inspirations include Kanye West and Kanye West.")
+u3 = User.create(username: "barkbark", password: "password", location: "San Luis Obispo, CA", bio: "I like sadder music. Slow stuff helps me concentrate.")
+u4 = User.create(username: "warriorprincess", password: "password", location: "San Diego, CA", bio: "I really only like oldies, new music is bad!")
+u5 = User.create(username: "thedykm", password: "password", location: "Seattle, WA", bio: "I can vibe to whatever, but let's be real, the less people who've heard of it the cooler it is.")
+u6 = User.create(username: "ridersofrray", password: "password", location: "Seattle, WA", bio: "When I find a song I like I play it for two weeks until I'm sick of it. I can't help it.")
+u7 = User.create(username: "ridethebus", password: "password", location: "San Francisco, CA", bio: "I like trance mixes. They help me stay up through the night working.")
+u8 = User.create(username: "strozzapreti", password: "password", location: "Montreal, Canada", bio: "I like funk and rhythm stuff, but I have a keen ear for jazz too. I've been known to enjoy a really deep rap song or two.")
+
+Track.destroy_all
+Track.create!(name: "Shelter", release_date: "2016/06/18", genre: "Electronica", user_id: u1.id)
+Track.create(name: "Moments", release_date: "2016/06/18", genre: "House", user_id: u2.id)
+Track.create(name: "Nevada", release_date: "2016/06/18", genre: "House", user_id: u3.id)
+Track.create(name: "Ava", release_date: "2016/06/18", genre: "Soundtrack", user_id: u3.id)
+Track.create(name: "Our Last Days as Children", release_date: "2016/06/18", genre: "Soft Rock", user_id: u3.id)
+Track.create(name: "The Night Passes the Earth to Day", release_date: "2016/06/18", genre: "Soft Rock", user_id: u4.id)
