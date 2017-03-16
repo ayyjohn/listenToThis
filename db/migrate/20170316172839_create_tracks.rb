57fea9d0d4e8570f311ef22abc4676956856f415
Alec Johnson <alecjohnson55@gmail.com>
2017-03-16 10:33:43 -0700

    add tracks to database

---
+++ db/migrate/20170316172839_create_tracks.rb
@@ -0,0 +1,16 @@
+class CreateTracks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tracks do |t|
+      t.string :name, null: false
+      t.string :image_url, null: false
+      t.date :release_date, null: false
+      t.text :description
+      t.string :genre, null: false
+      t.string :user_id, null: false
+      t.timestamps
+    end
+    add_index :tracks, :name
+    add_index :tracks, :genre
+    add_index :tracks, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314185327) do
+ActiveRecord::Schema.define(version: 20170316172839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tracks", force: :cascade do |t|
+    t.string   "name",         null: false
+    t.string   "image_url",    null: false
+    t.date     "release_date", null: false
+    t.text     "description"
+    t.string   "genre",        null: false
+    t.string   "user_id",      null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["genre"], name: "index_tracks_on_genre", using: :btree
+    t.index ["name"], name: "index_tracks_on_name", using: :btree
+    t.index ["user_id"], name: "index_tracks_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
