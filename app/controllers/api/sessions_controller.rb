10e1b90ef0fc5385bdf84f3cd55a31982c1d3d0f
Alec Johnson <alecjohnson55@gmail.com>
2017-03-16 13:00:14 -0700

    update to allow for future user show action with more detail

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,7 +7,7 @@ class Api::SessionsController < ApplicationController
 
     if @user
       log_in!(@user)
-      render "api/users/show"
+      render "api/users/current_user_show"
     else
       render json: ["invalid username/password combination"], status: 401
     end
