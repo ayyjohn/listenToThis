5b1bfda049cc39c0ea61bd60612988da5d3ccd29
Alec Johnson <alecjohnson55@gmail.com>
2017-03-19 13:05:06 -0700

    add billboard and components

--- frontend/components/track_detail.jsx
+++ frontend/components/track_detail.jsx
@@ -11,13 +11,30 @@ class TrackDetail extends React.Component {
       return (
         <section id="track-detail">
           <section className="track-detail-billboard">
+            <ul className="track-detail-billboard-0">
+              <li>
+                <ul className="track-detail-billboard-1">
+                  <li><i className="fa fa-play fa-2x" aria-hidden="true"></i></li>
+                  <li>
+                    <ul className="track-detail-billboard-2">
+                      <li className="track-detail-billboard-title">{ this.props.track.title }</li>
+                      <li className="track-detail-billboard-username">{ this.props.track.user.username }</li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <img
+                  src={ this.props.track.album_artwork_url }
+                  className="track-detail-album-artwork"></img>
+              </li>
+            </ul>
           </section>
           <p>{ this.props.track.title }</p>
           <p>{ this.props.track.release_date }</p>
           <p>{ this.props.track.description }</p>
           <p>{ this.props.track.genre }</p>
           <p>{ this.props.track.user.username }</p>
-          <img src={ this.props.track.album_artwork_url }></img>
         </section>
       );
     }
