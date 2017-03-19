e72b764a40eb6732e5a9eb8b2ab02571af1a1721
Alec Johnson <alecjohnson55@gmail.com>
2017-03-18 22:19:56 -0700

    style upload form

--- frontend/components/new_track_form.jsx
+++ frontend/components/new_track_form.jsx
@@ -88,9 +88,9 @@ class NewTrackForm extends React.Component {
   render() {
     return (
       <div className="upload-form">
-        <section>
-          <h1 className="upload-form-tagline">Spread the Music</h1>
-          <label for="new-mp3">Choose a file
+        <section className="choose-a-song">
+          <h1 className="upload-form-tagline">Spread your Music</h1>
+          <label className="new-mp3-label">Choose a song to upload
           <input
             name="new-mp3"
             className="new-mp3"
@@ -99,27 +99,63 @@ class NewTrackForm extends React.Component {
           </label>
         </section>
         <form className="new-track-form">
-          <label>Upload Album Artwork
-            <input
-              type="file"
-              onChange={ this.updateAlbumArtwork }/>
-            <img src={ this.state.album_artwork_url } />
-          </label>
-          <input
-            type="text"
-            placeholder="title"
-            onChange={ this.update("title") } />
-          <input
-            type="text"
-            placeholder="genre"
-            onChange={ this.update("genre") } />
-          <textarea
-            placeholder="describe your track"
-            onChange={ this.update("description") }></textarea>
-          <button
-            onClick={ this.cancelAndGoBack }>Cancel</button>
-          <button onClick={ this.handleSubmit }>Save</button>
-          { this.renderErrors() }
+          <ul className="new-track-form-0">
+            <li>
+              <h1 className="new-track-form-info-track-info">Track info</h1>
+            </li>
+            <ul className="new-track-form-1">
+              <li className="choose-album-artwork">
+                <label className="choose-album-artwork-label"><i className="fa fa-camera" aria-hidden="true"></i>Update image
+                  <input
+                    className="choose-album-artwork-input"
+                    type="file"
+                    onChange={ this.updateAlbumArtwork }/>
+                </label>
+                  <img
+                    src={ this.state.album_artwork_url }
+                    className="new-track-album-artwork"/>
+              </li>
+              <ul className="new-track-form-2">
+                <li className="new-track-form-title">
+                  <p>Title</p>
+                  <input
+                    type="text"
+                    className="name-your-track"
+                    placeholder="Name your track"
+                    onChange={ this.update("title") } />
+                </li>
+                <li>
+                  <p>Genre</p>
+                  <input
+                    type="text"
+                    placeholder="Genre"
+                    onChange={ this.update("genre") } />
+                </li>
+                <li>
+                  <p>Description</p>
+                  <textarea
+                    className="describe-your-track"
+                    placeholder="Describe your track"
+                    onChange={ this.update("description") }></textarea>
+                </li>
+                <li className="new-track-errors">
+                  { this.renderErrors() }
+                </li>
+              </ul>
+            </ul>
+            <ul className="new-track-cancel-and-save">
+              <li>
+                <button
+                  className="new-track-cancel-button"
+                  onClick={ this.cancelAndGoBack }>Cancel</button>
+              </li>
+              <li>
+                <button
+                  className="new-track-save-button"
+                  onClick={ this.handleSubmit }>Save</button>
+              </li>
+            </ul>
+          </ul>
         </form>
       </div>
     );
