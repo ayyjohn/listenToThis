b20ccf622e1834bc9b0da09959327b4af3c80a44
Alec Johnson <alecjohnson55@gmail.com>
2017-03-20 12:25:06 -0700

    add player container to app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import ReactHowler from 'react-howler';
 
 import NavBarContainer from './nav_bar_container';
+import PlayerContainer from './player_container';
 
 const App = ({ children }) => (
   <div id="app">
     <NavBarContainer/>
     { children }
+    <PlayerContainer />
   </div>
 );
 
