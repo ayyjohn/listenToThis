8e5ed21cc9e5daecd9ebca70b25bc7fbcbe63498
Alec Johnson <alecjohnson55@gmail.com>
2017-03-19 20:58:29 -0700

    add show action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,6 +11,15 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by_id(params[:id])
+
+    if @user
+      render :show
+    else
+      render json: ["That user doesn't exist"], status: 404
+    end
+  end
   private
 
   def user_params
