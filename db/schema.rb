3066b45d629e2b5d9ec58e2a287650325da07137
Alec Johnson <alecjohnson55@gmail.com>
2017-03-21 16:52:41 -0700

    add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320013935) do
+ActiveRecord::Schema.define(version: 20170321232344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "track_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id"], name: "index_comments_on_track_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "tracks", force: :cascade do |t|
     t.string   "title",                      null: false
