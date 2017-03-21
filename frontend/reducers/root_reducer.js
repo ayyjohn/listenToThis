3dac7ef23f2d90855fbf1cb075e26c06e4b01f04
Alec Johnson <alecjohnson55@gmail.com>
2017-03-20 17:28:19 -0700

    add queue reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import ErrorsReducer from './errors_reducer';
+import QueueReducer from './queue_reducer';
 import SessionReducer from './session_reducer';
 import TrackReducer from './track_reducer';
 import TracksReducer from './tracks_reducer';
@@ -7,6 +8,7 @@ import UserReducer from './user_reducer';
 
 const RootReducer = combineReducers({
   errors: ErrorsReducer,
+  queue: QueueReducer,
   session: SessionReducer,
   track: TrackReducer,
   tracks: TracksReducer,
