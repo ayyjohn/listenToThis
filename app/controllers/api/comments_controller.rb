5f08fee1b506ca3d7b06cec0d1a8802cdc0752c1
Alec Johnson <alecjohnson55@gmail.com>
2017-03-21 21:10:36 -0700

    unwrap from second controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,34 +1,30 @@
 class Api::CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
 
-  class CommentsController < ApplicationController
-
-    def create
-      @comment = Comment.new(comment_params)
-
-      if @comment.save
-        render :show
-      else
-        render json: @comment.errors.full_messages, status: 422
-      end
-
-    end
-
-    def index
-      @comments = Comment.where("track_id = ?", params[:trackId])
-      render :index
-    end
-
-    def destroy
-      @comment = Comment.find_by_id(params[:id])
-      @comment.delete
+    if @comment.save
       render :show
+    else
+      render json: @comment.errors.full_messages, status: 422
     end
 
-    private
+  end
 
-    def comment_params
-      params.require(:comment).permit(:body, :user_id, :track_id)
-    end
+  def index
+    @comments = Comment.where("track_id = ?", params[:trackId])
+    render :index
+  end
+
+  def destroy
+    @comment = Comment.find_by_id(params[:id])
+    @comment.delete
+    render :show
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :track_id)
   end
 
 end
