a58e51469c3f164e1ce785089219047271b72df6
Alec Johnson <alecjohnson55@gmail.com>
2017-03-23 16:36:08 -0700

    add update id action

--- frontend/actions/queue_actions.js
+++ frontend/actions/queue_actions.js
@@ -2,11 +2,17 @@ export const POPULATE_QUEUE = "POPULATE_QUEUE";
 export const UPDATE_INDEX = "UPDATE_INDEX";
 export const CLEAR_QUEUE = "CLEAR_QUEUE";
 export const UPDATE_PLAYING = "UPDATE_PLAYING";
+export const UPDATE_ID = "UPDATE_ID";
 
 export const populateQueue = (tracks, index) => ({
   type: POPULATE_QUEUE,
   tracks,
   index
+});
+
+export const updateId = id => ({
+  type: UPDATE_ID,
+  id
 });
 
 export const updateIndex = index => ({
