a150376913953a4aad36db6fc86a107270cfcbeb
Alec Johnson <alecjohnson55@gmail.com>
2017-06-13 16:23:28 -0700

    add search reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import ErrorsReducer from './errors_reducer';
 import QueueReducer from './queue_reducer';
+import SearchReducer from './search_reducer';
 import SessionReducer from './session_reducer';
 import TrackReducer from './track_reducer';
 import TracksReducer from './tracks_reducer';
@@ -9,6 +10,7 @@ import UserReducer from './user_reducer';
 const RootReducer = combineReducers({
   errors: ErrorsReducer,
   queue: QueueReducer,
+  searchParam: searchReducer,
   session: SessionReducer,
   track: TrackReducer,
   tracks: TracksReducer,
