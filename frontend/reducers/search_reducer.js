1e336d0adf32cb39bb2648841ce53672e2669b07
Alec Johnson <alecjohnson55@gmail.com>
2017-06-13 16:21:55 -0700

    initial commit

---
+++ frontend/reducers/search_reducer.js
@@ -0,0 +1,15 @@
+import { merge } from 'lodash';
+import { RECEIVE_SEARCHPARAM } from '../actions/seach_actions';
+
+const SearchReducer = (state = {}, action) => {
+  Object.freze(state);
+  let newState = merge({}, state);
+
+  switch(action.type) {
+    case RECEIVE_SEARCHTERM:
+      newState = merge({}, newState, action.searchParam);
+      return newState;
+    default:
+      return newState;
+  }
+}
